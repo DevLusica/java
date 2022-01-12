40e1da73b3a3b6c6eb57c0b5f5e5af5c4527dc7e
DevLusica <devlusica@gmail.com>
2022-01-12 23:06:01 +0900

    study

---
+++ study/src/Java100_variable_HelloWorld3.java
@@ -0,0 +1,6 @@
+public class Java100_variable_HelloWorld3 {
+    public static void main(String[] args) {
+        System.out.println(Short.BYTES);
+        System.out.println(Short.MAX_VALUE);
+    }
+}

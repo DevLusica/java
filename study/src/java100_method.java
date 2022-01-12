ba5848f93bb43c71dad28615822a35d6b6fb4bae
DevLusica <devlusica@gmail.com>
2022-01-13 05:43:52 +0900

    study

---
+++ study/src/java100_method.java
@@ -0,0 +1,9 @@
+public class java100_method {
+    public static int showMenu() {
+        int ret = 100;
+        return ret;
+    }
+    public static void main(String [] args) {
+        int res = showMenu();
+    }
+}
